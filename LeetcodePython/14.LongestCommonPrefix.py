080bee6ea93c2bf136e1f43c016e7de741b5b982
abhishtverma <abhishtverma555@gmail.com>
2026-05-13 00:49:47 +0530

    c

--- LeetcodePython/14.LongestCommonPrefix.py
+++ LeetcodePython/14.LongestCommonPrefix.py
@@ -5,7 +5,7 @@ class Solution(object):
         first = strs[0]
 
         for i in range (len(first)):
-         for j in strs[1:]:
+         for j in strs[1:]:#[1:]means from 1st position to last not 0th position
            if i >= len(j) or j[i]!= first[i]:
-              return first[:i]
+              return first[:i] #[:i]means upto/before the number not this 
         return first   
